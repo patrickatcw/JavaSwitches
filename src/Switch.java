d208610cc0d397526fda2ddc2ed08178c7e607b0
patrickatcw <pj8392@yahoo.com>
2018-02-12 09:56:35 -0500

    forgot default

--- src/Switch.java
+++ src/Switch.java
@@ -2,24 +2,27 @@ public class Switch {
 
     public static void main(String[] args) {
 
-       char switchLetter = 'D';
+        char switchLetter = 'Z';
 
-       switch(switchLetter) {
+        switch(switchLetter) {
 
-           case 'A':
-               System.out.println("The letter was an A");
-               break;
+            case 'A':
+                System.out.println("The letter was an A");
+                break;
 
-           case 'B':
-               System.out.println("The letter was a B");
+            case 'B':
+                System.out.println("The letter was a B");
 
-           case 'C': case 'D':
-               System.out.println("The letter was either a C or D");
+            case 'C': case 'D':
+                System.out.println("The letter was either a C or D");
 
-       }
+            default:
+                System.out.println("Letter not included in testing case");
+
+        }
 
     }
 
 }
 
-//result was The letter was either a C or D
+//result was Letter not included in testing case
