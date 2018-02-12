bef2dd0f0d1a46621358eb99d6bd5801326e46d0
patrickatcw <pj8392@yahoo.com>
2018-02-12 09:46:50 -0500

    changed program to examine character letters

--- src/Switch.java
+++ src/Switch.java
@@ -2,43 +2,24 @@ public class Switch {
 
     public static void main(String[] args) {
 
-        int switchValue = 10;    //var
+       char switchLetter = 'A';
 
-        switch(switchValue) {
+       switch(switchLetter) {
 
-            case 1:             //1 is testing against var
-                System.out.println("The number is 1");
-                break;
+           case 'A':
+               System.out.println("The letter was an A");
+               break;
 
-            case 2:             //2 is testing against var
-                System.out.println("The number is 2");
-                break;
+           case 'B':
+               System.out.println("The letter was a B");
 
-            case 3:             //3 is testing against var
-                System.out.println("The number is 3");
-                break;
+           case 'C': case 'D':
+               System.out.println("The letter was either a C or D");
 
-            case 4:             //4 is testing against var
-                System.out.println("The number is 4");
-                break;
-
-            case 5: case 6: case 7:         //grouping
-                System.out.println("The number was a 5, 6, or 7");
-                System.out.println("The number was actually a " + switchValue);
-                break;
-
-            default:                        //default
-                System.out.println("The number was not any of the case test numbers");
-                System.out.println("The number was actually " + switchValue);
-
-        }
+       }
 
     }
 
 }
 
-/*int switchValue = 10;
-result;
-The number was not any of the case test numbers
-The number was actually 10
-*/
+//result was The letter was A
